4ea4d9b939d9991c1357f679b694ab496bd1b76c
o.golovkina <golovkinaos@bimacad.ru>
2023-05-09 23:05:12 +0300

    Реализация аугментации через albumerations

---
+++ src/dataset_augmentations/dataset_augmentator.py
@@ -0,0 +1,16 @@
+import albumentations as A
+from albumentations import normalize
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform():
+    return A.Compose([
+        A.Flip(0.5),
+        A.RandomRotate90(0.5),
+        A.MotionBlur(p=0.2),
+        A.MedianBlur(blur_limit=3, p=0.1),
+        A.Blur(blur_limit=3, p=0.1),
+        ToTensorV2(p=1.0),
+    ], bbox_params={
+        'format': 'pascal_voc',
+        'label_fields': ['labels']
+    })
