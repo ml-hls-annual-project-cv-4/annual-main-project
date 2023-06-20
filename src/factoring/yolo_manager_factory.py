13b0f0ca4dc8ecc5e7774676a6f65e1c2f4c70e3
o.golovkina <golovkinaos@bimacad.ru>
2023-06-21 00:42:10 +0300

    Доработка фабрики

--- src/factoring/yolo_manager_factory.py
+++ src/factoring/yolo_manager_factory.py
@@ -1,21 +1,25 @@
 from src.contracts.deep_learning.dl_model_manager import DLModelManagerAbstract
 from src.contracts.factoring.deep_learning_manager_factory import DLManagerFactory
-
+from src.decoration.dl_logging_decorator import DLLManagerLoggingDecorator
+from src.decoration.yolo_data_prepare_decorator import YoloDataPrepareDecorator
+from src.deep_learning.yolo_model_manager import YoloModelManager
+from src.providing.logging_path_provider import LoggingPathFolderProvider
+from os import path
 
 class YoloManagerFactory(DLManagerFactory):
     @staticmethod
-    def get_dl_manager() -> DLModelManagerAbstract:
-        pass
+    def get_dl_manager(model_path: str) -> DLModelManagerAbstract:
+        return YoloModelManager(model_path)
 
     @staticmethod
-    def get_image_prep_decorator(dl_manager: DLModelManagerAbstract) -> DLModelManagerAbstract:
-        pass
+    def get_data_preparator(dl_manager: DLModelManagerAbstract) -> DLModelManagerAbstract:
+        return YoloDataPrepareDecorator(dl_manager)
 
     @staticmethod
     def get_logging_decorator(dl_manager: DLModelManagerAbstract) -> DLModelManagerAbstract:
-        pass
+        return DLLManagerLoggingDecorator(dl_manager, path.join(LoggingPathFolderProvider.get_path(), "dl_manager.log"))
 
     @staticmethod
-    def get_final_dl_manager() -> DLModelManagerAbstract:
+    def get_final_dl_manager(model_path: str) -> DLModelManagerAbstract:
         return YoloManagerFactory.get_logging_decorator(
-            YoloManagerFactory.get_image_prep_decorator(YoloManagerFactory.get_dl_manager()))
+            YoloManagerFactory.get_data_preparator(YoloManagerFactory.get_dl_manager(model_path)))
