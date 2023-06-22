b7085da50d405ff42029cec6685f5f348da55663
Ольга Головкина <GolovkinaOS@bimacad.ru>
2023-06-22 11:09:45 +0300

    Учет того, что у файла может быть разное расширение

--- tests/api/main.py
+++ tests/api/main.py
@@ -12,29 +12,39 @@ from src.factoring.yolo_manager_factory import YoloManagerFactory
 manager = YoloManagerFactory().get_final_dl_manager("models/detection_model.onnx")
 app = FastAPI()
 
+
 def get_filename_without_ext(filename):
     return path.splitext(filename)[0]
+
+
+def get_extension(filename):
+    return path.splitext(filename)[-1].replace(".", "")
+
 
 @app.post("/uploadfile/")
 async def upload_file(selected_file: UploadFile):
     pred = manager.predict(selected_file.file.read())
     filename = get_filename_without_ext(selected_file.filename)
+    ext = get_extension(selected_file.filename)
 
-    response = StreamingResponse(io.BytesIO(pred), media_type="image/jpg")
-    response.headers["Content-Disposition"] = f"attachment;filename={filename}_predicted.jpg"
+    response = StreamingResponse(io.BytesIO(pred), media_type=f"image/{ext}")
+    response.headers["Content-Disposition"] = f"attachment;filename={filename}_predicted.{ext}"
 
     return response
+
 
 def archivate(pred, files):
     zip_io = io.BytesIO()
 
     with zipfile.ZipFile(zip_io, mode='w', compression=zipfile.ZIP_DEFLATED) as temp_zip:
         for i, p in enumerate(pred):
-            img_filename = f"{get_filename_without_ext(files[i])}_predicted.jpg"
+            img_name = get_filename_without_ext(files[i])
+            img_ext = get_extension(files[i])
+
+            img_filename = f"{img_name}_predicted.{img_ext}"
             temp_zip.writestr(img_filename, p)
 
     return zip_io
-
 
 
 @app.post("/uploadfiles/")
@@ -46,7 +56,7 @@ async def upload_file(selected_files: list[UploadFile]):
     return StreamingResponse(
         iter([zip_io.getvalue()]),
         media_type="application/x-zip-compressed",
-        headers = { "Content-Disposition": f"attachment; filename=predictions.zip"}
+        headers={"Content-Disposition": f"attachment; filename=predictions.zip"}
     )
 
 
