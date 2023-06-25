e4a6abd6f60b0139203e4f19a2c568400d09d5e5
o.golovkina <golovkinaos@bimacad.ru>
2023-06-25 15:51:46 +0300

    Убрано для теста allow_credentials=True по мотивам топика https://stackoverflow.com/a/66460861

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
