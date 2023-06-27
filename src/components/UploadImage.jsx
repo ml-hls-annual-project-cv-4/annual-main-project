c96558ad4281dcb362861fe519a21209760a61ba
Dan <Danilaanis@gmail.com>
2023-06-27 19:54:08 +0300

    multiple files and archive addition

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -6,16 +6,16 @@ export const ImageUpload = () => {
   const [preview, setPreview] = useState();
   const [result, setResult] = useState();
   const [prediction, setPrediction] = useState();
+  const [downloadLink, setDownloadLink] = useState();
 
   useEffect(() => {
     if (!selectedFile) {
       setPreview(undefined);
       return;
     }
-
-    const objectUrl = URL.createObjectURL(selectedFile);
+    const objectUrl = selectedFile.map((file) => URL.createObjectURL(file));
     setPreview(objectUrl);
-
+    console.log(preview);
     return () => URL.revokeObjectURL(objectUrl);
   }, [selectedFile]);
 
@@ -24,22 +24,55 @@ export const ImageUpload = () => {
       setSelectedFile(undefined);
       return;
     }
-
-    setSelectedFile(e.target.files[0]);
+    var files = e.target.files;
+    var fileArray = Array.from(files).map(function (file) {
+      return file;
+    });
+    console.log(fileArray);
+    setSelectedFile(fileArray);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-    formData.append("selectedFile", selectedFile);
+    console.log(selectedFile);
+    if (selectedFile.length > 1) {
+      selectedFile.forEach(function (file) {
+        formData.append("selectedFiles", file);
+      });
+    } else {
+      selectedFile.forEach(function (file) {
+        formData.append("selectedFile", file);
+      });
+    }
     try {
       setResult(true);
-      const response = await axios.post("https://annualcv.ru:3000/uploadfile/", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-        responseType: 'blob'
-      });
+      if (selectedFile.length > 1) {
+        const response = await axios.post(
+          "https://annualcv.ru:3000/uploadfiles/",
+          formData,
+          {
+            headers: { "Content-Type": "multipart/form-data" },
+            responseType: "blob",
+          }
+        );
+        const url = URL.createObjectURL(response.data);
+        setDownloadLink(url);
+        setResult(false);
+        console.log(response);
+        return;
+      }
+      setDownloadLink(null)
+      const response = await axios.post(
+        "https://annualcv.ru:3000/uploadfile/",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+          responseType: "blob",
+        }
+      );
       setResult(false);
-      const imageUrl = URL.createObjectURL(new Blob([response.data]));
+      const imageUrl = URL.createObjectURL(response.data);
       setPrediction(imageUrl);
     } catch (error) {
       console.log(error);
@@ -52,7 +85,7 @@ export const ImageUpload = () => {
       <form onSubmit={handleSubmit} className="">
         <div className="bg-slate-300 bg-opacity-40 rounded-xl p-4 place-items-center flex flex-col">
           <input
-            accept='image/png, image/jpeg, image/jpg'
+            accept="image/png, image/jpeg, image/jpg"
             multiple
             type="file"
             onChange={onSelectFile}
@@ -64,7 +97,16 @@ export const ImageUpload = () => {
             hover:file:cursor-pointer hover:file:bg-amber-50
             hover:file:text-amber-700"
           />
-          {selectedFile && <img className="h-auto max-h-96 max-w-full" src={preview} alt="Preview" />}
+          <div className="flex space-x-2">
+            {selectedFile &&
+              preview?.map((image) => (
+                <img
+                  className="h-auto max-h-96 max-w-full"
+                  src={image}
+                  alt="Preview"
+                />
+              ))}
+          </div>
 
           {selectedFile ? (
             <input
@@ -84,7 +126,9 @@ export const ImageUpload = () => {
 
           {result && (
             <div>
-              <label className="text-white text-xl">Файл успешно загружен, ожидайте...</label>
+              <label className="text-white text-xl">
+                Файл успешно загружен, ожидайте...
+              </label>
               <svg
                 aria-hidden="true"
                 className="mx-auto mt-2 mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
@@ -97,7 +141,25 @@ export const ImageUpload = () => {
             </div>
           )}
 
-          {prediction && <img src={prediction} className="my-3 h-auto max-h-96 max-w-full" alt="Prediction" />}
+          {prediction && (
+            <img
+              src={prediction}
+              className="my-3 h-auto max-h-96 max-w-full"
+              alt="Prediction"
+            />
+          )}
+          {downloadLink && (
+            <a
+              className="mt-3 text-grey-500
+              py-2 px-6
+              rounded-full border-0
+              text-lg font-medium
+              bg-blue-50 text-amber-400"
+              href={downloadLink}
+            >
+              Скачать архив
+            </a>
+          )}
         </div>
       </form>
     </div>
